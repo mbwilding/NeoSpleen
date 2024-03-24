33e897c095a8e101ad62c79bf222c2b973a73909
Matthew Wilding <mbwilding@gmail.com>
2024-03-25 05:33:53 +0800

    Updated Generate.sh

--- Generate.sh
+++ Generate.sh
@@ -3,6 +3,9 @@
 name="NeoSpleen"
 input="$name.sfd"
 output="$name.ttf"
+outputnf="${name}NerdFont-Regular.ttf"
 
+rm "$output"
+rm "$outputnf"
 fontforge -lang=ff -c "Open(\"$input\"); Generate(\"$output\")" "$input" "$output"
-fontforge -script Patcher/font-patcher NeoSpleen.ttf -c --boxdrawing --removeligs --progressbars
+fontforge -script Patcher/font-patcher "$output" -c --boxdrawing --removeligs --progressbars
