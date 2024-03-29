55b91b65698366f3154f2421d423b05885baea10
Matthew Wilding <mbwilding@gmail.com>
2024-03-30 01:37:59 +0800

    [ci skip] NVIM

--- .nvim/Showcase [Linux].lua
+++
@@ -1,9 +0,0 @@
-return {
-	terminal_commands = {
-		[1] = {
-			"sudo install python3",
-			"pip3 install Pillow",
-			"python3 generate_image.py",
-		}
-	}
-}
--- .nvim/Showcase [Windows].lua
+++
@@ -1,9 +0,0 @@
-return {
-	terminal_commands = {
-		[1] = {
-			"winget install python3 --accept-package-agreements --accept-source-agreements",
-			"pip3 install Pillow",
-			"python3 generate_image.py",
-		}
-	}
-}
