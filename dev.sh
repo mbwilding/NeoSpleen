0d4a2fee6364dbc991810e9560780fb65cdb475c
Matthew Wilding <mbwilding@gmail.com>
2025-05-12 14:18:11 +0800

    [ci skip]

--- dev.sh
+++ dev.sh
@@ -4,12 +4,18 @@ set -e
 
 # Generate Fonts
 FONT_NAME="NeoSpleen"
+FONT_DIR="fonts"
 sfd="${FONT_NAME}.sfd"
 formats=("ttf") # TODO: Add "woff2"
 for format in "${formats[@]}"; do
-    output="${FONT_NAME}-Regular.${format}"
+    output="${FONT_DIR}/${FONT_NAME}.${format}"
     fontforge -lang=ff -c "Open(\"${sfd}\"); Generate(\"${output}\")"
 done
 
 # Generate Weight Fonts
 fontforge -lang=py -script generate_font_weights.py
+
+# Clean up
+for format in "${formats[@]}"; do
+    rm "${FONT_DIR}/${FONT_NAME}.${format}"
+done
--- generate_font_weights.py
+++ generate_font_weights.py
@@ -1,7 +1,7 @@
 import fontforge
 
-def generate_font(font_type: str, font_weight: int, font_scale: int) -> None:
-    font = fontforge.open("NeoSpleen-Regular.ttf")
+def generate_font(font_type: str, font_weight: int) -> None:
+    font = fontforge.open("fonts/NeoSpleen.ttf")
     font.selection.all()
 
     # Set font metadata
@@ -10,15 +10,22 @@ def generate_font(font_type: str, font_weight: int, font_scale: int) -> None:
     font.weight = font_type
     font.os2_weight = font_weight
     font.appendSFNTName("English (US)", "SubFamily", font_type)
-
-    # Modify weight of glyphs
+    font_scale = 1 + (font_weight - 400) * (31 / 300)
     font.changeWeight(font_scale, "LCG", 0, 0, "squish")
-
-    # Generate the new font file
-    output_file = f"NeoSpleen-{font_type}.ttf"
+    output_file = f"fonts/NeoSpleen-{font_type}.ttf"
     font.generate(output_file)
-    print(font)
+    print("Type: ", font_type)
+    print("Weight: ", font_weight)
+    print("Scale: ", font_scale)
     font.close()
 
 if __name__ == "__main__":
-    generate_font("Bold", 700, 32)
+    generate_font("Thin", 100)
+    generate_font("ExtraLight", 200)
+    generate_font("Light", 300)
+    generate_font("Regular", 400)
+    generate_font("Medium", 500)
+    generate_font("SemiBold", 600)
+    generate_font("Bold", 700)
+    generate_font("ExtraBold", 800)
+    generate_font("Black", 900)
