0269b4e1735d01a9197f6453d76575bf24ee7d17
Matthew Wilding <mbwilding@gmail.com>
2025-05-12 14:30:39 +0800

    [ci skip]

---
+++ generate_fonts.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import sys
+import fontforge
+
+def generate_font(font_type: str, font_weight: int, ext: str) -> None:
+    font = fontforge.open("NeoSpleen.sfd")
+    font.selection.all()
+    font.fontname = f"NeoSpleen-{font_type}"
+    font.fullname = f"NeoSpleen {font_type}"
+    font.weight = font_type
+    font.os2_weight = font_weight
+    font.appendSFNTName("English (US)", "SubFamily", font_type)
+    output_file = f"fonts/NeoSpleen-{font_type}.{ext}"
+
+    font_scale = 1 + (font_weight - 400) * (31 / 300)
+    if font_scale != 1.0:
+        font.changeWeight(font_scale, "LCG", 0, 0, "squish")
+
+    font.generate(output_file)
+    print(f"Generated {output_file}:")
+    print("  Type:", font_type)
+    print("  Weight:", font_weight)
+    print("  Scale:", font_scale)
+    font.close()
+
+def main():
+    formats = sys.argv[1:]
+    if not formats:
+        formats = ["ttf"]
+
+    font_weights = [
+        # ("Thin", 100),
+        # ("ExtraLight", 200),
+        # ("Light", 300),
+        ("Regular", 400),
+        ("Medium", 500),
+        # ("SemiBold", 600),
+        ("Bold", 700),
+        # ("ExtraBold", 800),
+        # ("Black", 900)
+    ]
+
+    for ext in formats:
+        for font_type, font_weight in font_weights:
+            generate_font(font_type, font_weight, ext)
+
+if __name__ == "__main__":
+    main()
