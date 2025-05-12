63e582ef755209734472e1d03c012ed7d044348b
Matthew Wilding <mbwilding@gmail.com>
2025-05-12 11:47:02 +0800

    [ci skip]

--- generate_font_weights.py
+++ generate_font_weights.py
@@ -1,12 +1,12 @@
 import fontforge
 
 # Generate bold weight
-font = fontforge.open('NeoSpleen-Regular.ttf')
+font = fontforge.open("NeoSpleen-Regular.ttf")
 font.selection.all()
-font.fontname = 'NeoSpleen-Bold'
-font.fullname = 'NeoSpleen Bold'
-font.weight = 'Bold'
+font.fontname = "NeoSpleen-Bold"
+font.fullname = "NeoSpleen Bold"
+font.weight = "Bold"
 font.changeWeight(32, "LCG", 0, 0, "squish")
-font.generate('NeoSpleen-Bold.ttf')
+font.generate("NeoSpleen-Bold.ttf")
 print(font)
 font.close()
