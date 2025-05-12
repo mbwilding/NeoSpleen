bc2bd742ebbd94f39eec909346c28f9c273d060f
Matthew Wilding <mbwilding@gmail.com>
2025-05-12 15:26:06 +0800

    [ci skip]

--- dev.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-formats=("ttf") # TODO: Add "woff2"
-fontforge -lang=py -script generate_fonts.py "${formats[@]}"
--- generate_nerd_fonts.py
+++ generate_nerd_fonts.py
@@ -1,57 +1,47 @@
 #!/usr/bin/env python3
 
+import glob
 import os
+import shutil
 import subprocess
 import urllib.request
 import zipfile
-import glob
-import shutil
 
-zip_url = "https://github.com/ryanoasis/nerd-fonts/raw/master/FontPatcher.zip"
-zip_file = "FontPatcher.zip"
-patcher_dir = "NerdFontPatcher"
-fonts_dir = "./fonts"
+ZIP_URL = "https://github.com/ryanoasis/nerd-fonts/raw/master/FontPatcher.zip"
+ZIP_FILE = "FontPatcher.zip"
+PATCHER_DIR = "NerdFontPatcher"
+FONTS_DIR = "./fonts"
 
-if not os.path.exists(zip_file):
-    print("Downloading FontPatcher.zip...")
-    urllib.request.urlretrieve(zip_url, zip_file)
-    print("Download complete")
-else:
-    print("FontPatcher.zip already exists, skipping download")
-
-if not os.path.isdir(patcher_dir):
-    print("Unzipping FontPatcher.zip into", patcher_dir)
-    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
-        zip_ref.extractall(patcher_dir)
-    print("Unzip complete.")
-else:
-    print(f"{patcher_dir} already exists, skipping unzip")
+if not os.path.isdir(PATCHER_DIR):
+    urllib.request.urlretrieve(ZIP_URL, ZIP_FILE)
+    with zipfile.ZipFile(ZIP_FILE, 'r') as zip_ref:
+        zip_ref.extractall(PATCHER_DIR)
+    os.remove(ZIP_FILE)
 
 font_files = [
-    f for f in glob.glob(os.path.join(fonts_dir, "*.ttf"))
+    f for f in glob.glob(os.path.join(FONTS_DIR, "*.ttf"))
     if "NerdFont" not in os.path.basename(f)
 ]
 if not font_files:
-    print("No .ttf font files found in the ./fonts folder")
-else:
-    for font_path in font_files:
-        base_name = os.path.splitext(os.path.basename(font_path))[0]
-        print(f"Processing font: {base_name}")
+    raise FileNotFoundError("No .ttf files found in the 'fonts' directory")
 
-        cmd = [
-            "fontforge",
-            "-script",
-            os.path.join(patcher_dir, "font-patcher"),
-            font_path,
-            "--complete",
-            "--boxdrawing",
-            "--no-progressbars",
-            "--quiet"
-        ]
+for font_path in font_files:
+    base_name = os.path.splitext(os.path.basename(font_path))[0]
+    print(f"Processing font: {base_name}")
 
-        subprocess.run(cmd, check=True)
+    cmd = [
+        "fontforge",
+        "-script",
+        os.path.join(PATCHER_DIR, "font-patcher"),
+        font_path,
+        "--complete",
+        "--boxdrawing",
+        "--no-progressbars",
+        "--quiet"
+    ]
 
-    for ttf_file in glob.glob("*.ttf"):
-        target_path = os.path.join(fonts_dir, os.path.basename(ttf_file))
-        print(f"Moving {ttf_file} to {target_path}")
-        shutil.move(ttf_file, target_path)
+    subprocess.run(cmd, check=True)
+
+for ttf_file in glob.glob("*.ttf"):
+    target_path = os.path.join(FONTS_DIR, os.path.basename(ttf_file))
+    shutil.move(ttf_file, target_path)
