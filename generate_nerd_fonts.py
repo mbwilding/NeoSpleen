e5013844a5b596116d43f159587fece536150e29
Matthew Wilding <mbwilding@gmail.com>
2025-05-14 23:41:12 +0800

    2025-05-14-23-41-12

--- generate_nerd_fonts.py
+++ generate_nerd_fonts.py
@@ -44,7 +44,6 @@ def main():
 
         subprocess.run(cmd, check=True)
 
-
 for ttf_file in glob.glob("*.ttf"):
     target_path = os.path.join(FONTS_DIR, os.path.basename(ttf_file))
     shutil.move(ttf_file, target_path)
